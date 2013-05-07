9cc04fc7df3d10d1ae005655cd7c3e1111d6f724
Özgür Akgün <ozgurakgun@gmail.com>
2013-05-07 14:41:52 +0100

    no need to duplicate param files

--- test/solving/sarays/optimising/run.sh
+++ test/solving/sarays/optimising/run.sh
@@ -1,3 +1,6 @@
 
+cp ../*.param .
 parallel -u " savilerow -in-eprime sarays.eprime -in-param {} -out-minion {.}.minion " ::: *.param
 parallel -u " minion {} | tee {.}.minion-out " ::: *.minion
+rm *.param
+
